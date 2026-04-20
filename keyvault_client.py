05a82bb690f556fdc22da1c59d6ea1d1739d316e
sourav82 <sourav82@users.noreply.github.com>
2026-04-20 16:10:49 +0100

    Update keyvault_client.py

--- keyvault_client.py
+++ keyvault_client.py
@@ -1,1 +1,11 @@
 
+from azure.identity import DefaultAzureCredential
+from azure.keyvault.secrets import SecretClient
+from app.config import KEYVAULT_URL
+
+credential = DefaultAzureCredential()
+client = SecretClient(vault_url=KEYVAULT_URL, credential=credential)
+
+
+def get_secret(name: str) -> str:
+    return client.get_secret(name).value
